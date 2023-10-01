2ea0bf7ad4e1e32fbbff33d2870d38eecd0bf74f
ShuntaroKishida <137754393+ShuntaroKishida@users.noreply.github.com>
2023-10-01 12:29:46 +0900

    ひとまずcommit

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,24 @@
-import Layout from '../components/layout'
-import MyImage from '../components/image'
+import { useState } from "react";
+import Layout from "../components/layout";
+import useSWR from "swr";
 
 export default function Home() {
+  const [address, setAddress] = useState("/api/hello");
+  const { data, error } = useSWR(address);
+
+  const onChange = (e) => {
+    const newAddress = "/api/hello?id=" + e.target.value;
+    setAddress(newAddress);
+  };
+
   return (
     <div>
       <Layout header="Next.js" title="Top page.">
-      <div className="alert alert-primary text-center">
-        <h5 className="mb-4">Welcome to next.js!</h5>
-        <MyImage fname="image.jpeg" size="300" />
-      </div>
+        <div className="alert alert-primary text-center">
+          <h5 className="mb-4">{JSON.stringify(data)}</h5>
+          <input type="number" className="form-control" onChange={onChange} />
+        </div>
       </Layout>
     </div>
-  )
+  );
 }
